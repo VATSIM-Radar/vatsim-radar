c85f06d27549e79eb7aa67be80bf03cc9863410e
daniluk4000 <daniluk4000@outlook.com>
2024-04-21 18:54:44 +0300

    Updated Privacy Policy

--- src/pages/privacy-policy.vue
+++ src/pages/privacy-policy.vue
@@ -16,7 +16,7 @@
                         Services on this domain and subdomains would be described from later on as Vatsim Radar
                     </li>
                     <li>
-                        This Privacy Policy was last updated on 15 April 2024 (revision 1)
+                        This Privacy Policy was last updated on 21 April 2024 (revision 2)
                     </li>
                     <li>
                         User agrees to Privacy Policy by using Vatsim Radar
@@ -31,7 +31,8 @@
 
                 <ol>
                     <li>
-                        Vatsim Radar may store your data in persistent DataBase after linking of Vatsim and/or Navigraph accounts
+                        Vatsim Radar may store your data in persistent DataBase after linking of Vatsim, Navigraph and
+                        Discord accounts
                     </li>
                     <li>
                         Personal data, stored by Vatsim Radar, includes:
@@ -44,10 +45,15 @@
                                 Navigraph user info: account token, ID and Navigraph Unlimited subscription status
                             </li>
                             <li>
-                                User's preferences, including, but not limited to, Settings, Favourite users, Filters, and other non-3rd party related preferences
+                                Discord user info: ID and relation with Vatsim account
                             </li>
                             <li>
-                                User's preferences data does not include behaviour tracking and does not include any information that may be used to analyze user's identity
+                                User's preferences, including, but not limited to, Settings, Favourite users, Filters,
+                                and other non-3rd party related preferences
+                            </li>
+                            <li>
+                                User's preferences data does not include behaviour tracking and does not include any
+                                information that may be used to analyze user's identity
                             </li>
                         </ol>
                     </li>
@@ -61,7 +67,8 @@
                         Vatsim Radar may use Cookie, Local Storage or other browser features
                     </li>
                     <li>
-                        Data, stored in browser, includes, but not limited to, authorization token, preferences (examples: selected filters, map layers, last map position, e.t.c.)
+                        Data, stored in browser, includes, but not limited to, authorization token, preferences
+                        (examples: selected filters, map layers, last map position, e.t.c.)
                     </li>
                 </ol>
             </li>
@@ -73,10 +80,12 @@
                         Vatsim Radar does not share any data, described in 2. and 3. with any external service
                     </li>
                     <li>
-                        Vatsim Radar does not include any analytics scripts or hidden pixels that can be used to track user
+                        Vatsim Radar does not include any analytics scripts or hidden pixels that can be used to track
+                        user
                     </li>
                     <li>
-                        All personal data can not be accessed via API, excluding accessing his own data by authorized user
+                        All personal data can not be accessed via API, excluding accessing his own data by authorized
+                        user
                     </li>
                 </ol>
             </li>
@@ -85,19 +94,27 @@
 
                 <ol>
                     <li>
-                        User may delete Persistent stored data, described in 2., by using Delete Account button in Vatsim Radar Settings
+                        User may delete Persistent stored data, described in 2., by using Delete Account button in
+                        Vatsim Radar Settings
                     </li>
                     <li>
                         Data will be automatically permanently deleted withing 24 hours from deletion request
                     </li>
                     <li>
-                        User can't disallow using of Browser stored data, such as Cookies, since they are required for Vatsim Radar operation
+                        User can't disallow using of Browser stored data, such as Cookies, since they are required for
+                        Vatsim Radar operation
                     </li>
                     <li>
                         In order for service to operate normally, JavaScript shall not be disabled
                     </li>
                     <li>
                         User agrees to storing of his data by using Vatsim Radar
+                    </li>
+                    <li>
+                        Vatsim Radar will comply with deletion requests that was initialized by users of 3rd party
+                        systems which data is
+                        stored in Persistent Database, such as Vatsim, Navigraph or Discord, within 30 days from their
+                        request
                     </li>
                 </ol>
             </li>
