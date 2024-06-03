1b2851f592eb14703fcbc78a592484b69ba779ed
daniluk4000 <daniluk4000@outlook.com>
2024-06-03 23:40:46 +0300

    chore: fix discord auth

--- src/server/plugins/user.ts
+++ src/server/plugins/user.ts
@@ -7,11 +7,13 @@ export default defineNitroPlugin(app => {
     const config = useRuntimeConfig();
 
     app.hooks.hook('request', async event => {
-        if ((!config.ACCESS_BY_DISCORD_ROLES || event.path.startsWith('/api')) && (event.path.startsWith('/api/auth') || (event.path.startsWith('/api/data') && !event.path.includes('navigraph')))) return;
+        const isPage = !event.path.startsWith('/api') && !event.path.startsWith('/layers') && !event.path.startsWith('/static');
+
+        if ((!config.ACCESS_BY_DISCORD_ROLES || !isPage) && (event.path.startsWith('/api/auth') || (event.path.startsWith('/api/data') && !event.path.includes('navigraph')))) return;
 
         event.context.user = await findAndRefreshFullUserByCookie(event);
 
-        if (config.ACCESS_BY_DISCORD_ROLES && !event.path.startsWith('/api')) {
+        if (config.ACCESS_BY_DISCORD_ROLES && isPage) {
             const discordId = event.context.user?.discordId;
             event.context.authRestricted = true;
             if (discordId) {
