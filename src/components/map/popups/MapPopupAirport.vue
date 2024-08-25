5bf5162278c6024d1a406c8ac45a338f61a157f5
daniluk4000 <daniluk4000@outlook.com>
2024-08-25 22:02:26 +0300

    fix: pause airports update while in progress

--- src/components/map/popups/MapPopupAirport.vue
+++ src/components/map/popups/MapPopupAirport.vue
@@ -277,11 +277,23 @@ const vatInfo = computed(() => {
     return data.value?.vatInfo;
 });
 
+let updateInProgress = false;
+
 watch(dataStore.vatsim.updateTimestamp, async () => {
-    props.overlay.data.airport = {
-        ...props.overlay.data.airport,
-        ...await $fetch<VatsimAirportData>(`/api/data/vatsim/airport/${ props.overlay.key }?requestedDataType=2`),
-    };
+    if (updateInProgress) return;
+    updateInProgress = true;
+    try {
+        props.overlay.data.airport = {
+            ...props.overlay.data.airport,
+            ...await $fetch<VatsimAirportData>(`/api/data/vatsim/airport/${ props.overlay.key }?requestedDataType=2`),
+        };
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        updateInProgress = false;
+    }
 });
 
 onMounted(() => {
