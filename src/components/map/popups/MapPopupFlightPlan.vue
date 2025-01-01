bdba68e07d827ffd2975952c5f1976543dd74900
daniluk4000 <daniluk4000@outlook.com>
2025-01-01 22:25:22 +0300

    fix lint

--- src/components/map/popups/MapPopupFlightPlan.vue
+++ src/components/map/popups/MapPopupFlightPlan.vue
@@ -94,7 +94,6 @@ import type { PropType } from 'vue';
 import CommonCopyInfoBlock from '~/components/common/blocks/CommonCopyInfoBlock.vue';
 import CommonInfoBlock from '~/components/common/blocks/CommonInfoBlock.vue';
 import CommonPilotDestination from '~/components/common/vatsim/CommonPilotDestination.vue';
-import { getAirlineFromCallsign } from '~/composables';
 
 const props = defineProps({
     flightPlan: {
