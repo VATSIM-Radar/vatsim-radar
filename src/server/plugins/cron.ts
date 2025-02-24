9d42f9d80a16feda6b2f71c5453c8ed7379b3495
daniluk4000 <daniluk4000@outlook.com>
2025-02-24 23:27:15 +0300

    Fix VG combined

--- src/server/plugins/cron.ts
+++ src/server/plugins/cron.ts
@@ -1,9 +1,18 @@
 import { defineCronJob } from '~/utils/backend';
 import { initNavigraph } from '~/utils/backend/navigraph-db';
 import { setupRedisDataFetch } from '~/utils/backend/tasks';
+import { radarStorage } from '~/utils/backend/storage';
+import { getRedis } from '~/utils/backend/redis';
+
+const redisSubscriber = getRedis();
 
 export default defineNitroPlugin(app => {
     setupRedisDataFetch();
+
+    redisSubscriber.subscribe('vatglassesActive');
+    redisSubscriber.on('message', (_, message) => {
+        radarStorage.vatglasses.activeData = message;
+    });
 
     defineCronJob('15 */2 * * *', initNavigraph);
 });
