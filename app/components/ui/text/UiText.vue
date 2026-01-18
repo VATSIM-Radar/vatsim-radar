52eb1649f17073b45c22d6bcfd5724f0e9875e1b
daniluk4000 <daniluk4000@outlook.com>
2026-01-18 16:23:16 +0300

    wip

---
+++ app/components/ui/text/UiText.vue
@@ -0,0 +1,50 @@
+<template>
+    <component
+        :is="getTag"
+        class="text"
+        :class="[`text--type-${ type }`]"
+    >
+        <slot/>
+    </component>
+</template>
+
+<script setup lang="ts">
+export type UiTextTypes =
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6-upper'
+    | '1b'
+    | '2b-medium'
+    | '2b'
+    | '3b'
+    | '3b-medium'
+    | '3b-medium-alt'
+    | 'caption-light'
+    | 'caption'
+    | 'caption-medium'
+    | 'caption-medium-alt';
+
+defineProps({
+    type: {
+        type: String as PropType<any>,
+    },
+    tag: {
+        type: String,
+    },
+});
+
+defineSlots<{ default: () => any }>();
+
+const getTag = computed(() => {
+
+});
+</script>
+
+<style scoped lang="scss">
+.text {
+
+}
+</style>
