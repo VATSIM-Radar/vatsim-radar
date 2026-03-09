952dcfeeea877864be87bdb6d5e678cdfc5710f1
daniluk4000 <daniluk4000@outlook.com>
2026-03-09 15:20:15 +0300

    Move NOTAMs to a separate server

--- app/utils/server/notams.ts
+++ app/utils/server/notams.ts
@@ -67,31 +67,6 @@ export async function getAirportNotams(icao: string, formatted: true): Promise<s
 export async function getAirportNotams(icao: string, formatted?: false): Promise<VatsimAirportDataNotam[]>;
 export async function getAirportNotams(icao: string, formatted?: boolean): Promise<VatsimAirportDataNotam[] | string[]>;
 export async function getAirportNotams(icao: string, formatted?: boolean): Promise<VatsimAirportDataNotam[] | string[]> {
-    const config = useRuntimeConfig();
-    const clientId = config.FAA_NOTAMS_CLIENT_ID ?? process.env.FAA_NOTAMS_CLIENT_ID;
-    const clientSecret = config.FAA_NOTAMS_CLIENT_SECRET ?? process.env.FAA_NOTAMS_CLIENT_SECRET;
-
-    if (!clientId || !clientSecret) return [];
-
-    let faaToken = await getRedisSync('faa-nms-token');
-    if (!faaToken) {
-        const params = new URLSearchParams();
-        params.set('grant_type', 'client_credentials');
-
-        const faaTokenRequest = await $fetch<{
-            access_token: string;
-            expires_in: string;
-        }>('https://api-nms.aim.faa.gov/v1/auth/token', {
-            method: 'POST',
-            body: params,
-            headers: {
-                Authorization: `Basic ${ btoa(`${ clientId }:${ clientSecret }`) }`,
-            },
-        });
-        faaToken = faaTokenRequest.access_token;
-        await setRedisSync('faa-nms-token', faaToken, 1000 * (Number(faaTokenRequest.expires_in) - 1));
-    }
-
     const cachedNotamsRedis = await getRedisSync(`${ icao }-notams`);
     const cachedNotams: VatsimAirportDataNotam[] | null = cachedNotamsRedis ? JSON.parse(cachedNotamsRedis) : null;
 
@@ -99,46 +74,11 @@ export async function getAirportNotams(icao: string, formatted?: boolean): Promi
         return formatted ? cachedNotams.map(x => x.formattedText as string).filter(x => !!x) : cachedNotams;
     }
 
-    const notams: VatsimAirportDataNotam[] = [];
-    const notamsText: string[] = [];
-
-    const url = new URL('https://api-nms.aim.faa.gov/nmsapi/v1/notams');
-    url.searchParams.set('nmsResponseFormat', 'geoJson');
-    url.searchParams.set('location', icao);
-
-    const data = await $fetch<FAANotamsResponse>(url.toString(), {
-        headers: {
-            Authorization: `Bearer ${ faaToken }`,
-            nmsResponseFormat: 'GEOJSON',
-        },
+    const request = await $fetch<VatsimAirportDataNotam[]>(`http://notams.vatsim-radar.com:3000/notams/${ icao }`, {
+        timeout: 5000,
     });
 
-    if (!Array.isArray(data?.data?.geojson)) return [];
+    await setRedisSync(`${ icao }-notams`, JSON.stringify(request), 1000 * 60 * 5);
 
-    for (const notam of data.data.geojson.toSorted((a, b) => new Date(b.properties.coreNOTAMData.notam.effectiveStart).getTime() - new Date(a.properties.coreNOTAMData.notam.effectiveStart).getTime())) {
-        const data = notam.properties.coreNOTAMData.notam;
-
-        if (data.effectiveEnd !== 'PERM' && data.effectiveEnd.startsWith('20') && new Date(data.effectiveEnd).getTime() < Date.now()) continue;
-
-        const formattedText = notam.properties.coreNOTAMData.notamTranslation?.find(x => x.type === 'ICAO')?.formattedText;
-
-        notams.push({
-            number: data.number,
-            type: data.type as any,
-            issued: data.issued,
-            effectiveFrom: data.effectiveStart,
-            effectiveTo: data.effectiveEnd,
-            text: data.text,
-            formattedText: formattedText,
-            classification: data.classification as any,
-            schedule: data.schedule || undefined,
-            scope: data.scope,
-        });
-
-        if (formatted && formattedText) notamsText.push(formattedText);
-    }
-
-    await setRedisSync(`${ icao }-notams`, JSON.stringify(notams), 1000 * 60 * 5);
-
-    return formatted ? notamsText : notams;
+    return formatted ? request.map(x => x.formattedText as string).filter(x => !!x) : request;
 }
