091a701b78548deef5ecf94512416a9865f066ca
daniluk4000 <daniluk4000@outlook.com>
2026-04-09 10:34:31 +0300

    Better handle bulk notams error

--- server/api/data/notams.ts
+++ server/api/data/notams.ts
@@ -1,20 +1,26 @@
-import { handleH3Error } from '~/utils/server/h3';
+import { handleH3Error, handleH3Exception } from '~/utils/server/h3';
 import { getAirportNotams } from '~/utils/server/notams';
 
 export default defineEventHandler(async event => {
-    const query = getQuery(event);
-    const isShort = !query.full;
-    const icao = getQuery(event).icao;
+    try {
+        const query = getQuery(event);
+        const isShort = !query.full;
+        const icao = getQuery(event).icao;
 
-    if (!icao || typeof icao !== 'string') {
-        return handleH3Error({
-            event,
-            statusCode: 400,
-            data: 'icao GET-param is required',
-        });
+        if (!icao || typeof icao !== 'string') {
+            return handleH3Error({
+                event,
+                statusCode: 400,
+                data: 'icao GET-param is required',
+            });
+        }
+
+        const icaos = icao.split(',');
+
+        return Object.fromEntries(await Promise.all(icaos.map(async icao => [icao, await getAirportNotams(icao, isShort).catch(() => ([]))])));
     }
-
-    const icaos = icao.split(',');
-
-    return Object.fromEntries(await Promise.all(icaos.map(async icao => [icao, await getAirportNotams(icao, isShort)])));
+    catch (e) {
+        handleH3Exception(event, e);
+        return;
+    }
 });
