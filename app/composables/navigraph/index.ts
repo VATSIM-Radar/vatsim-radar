93dc17769cfb04621e4ae1b54a95a28b36605eac
daniluk4000 <daniluk4000@outlook.com>
2026-05-04 20:59:13 +0300

    Waypoints parse crash fix

--- app/composables/navigraph/index.ts
+++ app/composables/navigraph/index.ts
@@ -577,7 +577,7 @@ export async function getFlightPlanWaypoints({
                 if (arrStar || arrApproach || star !== -1) {
                     starInit = true;
 
-                    const starProcedure = arrStar?.procedure ?? await getNavigraphAirportProcedure('stars', arrival, star);
+                    const starProcedure: NavDataProcedure<NavigraphNavDataStar> | null = arrStar?.procedure ?? await getNavigraphAirportProcedure('stars', arrival, star);
                     let arrivalProcedures = (arrRunway && await getNavigraphAirportShortProceduresForKey('approaches', arrival));
 
                     if (!arrivalProcedures && Array.isArray(starProcedure?.procedure.runways) && starProcedure?.procedure.runways.length === 1 && starProcedure?.procedure.runways[0]) {
@@ -630,7 +630,7 @@ export async function getFlightPlanWaypoints({
                                 : null;
 
                         if (fetchedProcedure) {
-                            const transition = fetchedProcedure?.transitions.find(x => arrApproach?.transitions.includes(x.name) || x.name === starProcedure.waypoints[starProcedure.waypoints.length - 1]?.identifier);
+                            const transition = fetchedProcedure?.transitions.find(x => arrApproach?.transitions.includes(x.name) || x.name === starProcedure?.waypoints[starProcedure.waypoints.length - 1]?.identifier);
 
                             if (transition) {
                                 deleteDoubleWaypoint(transition?.waypoints[0]?.identifier ?? '');
@@ -795,6 +795,8 @@ export async function getFlightPlanWaypoints({
             const vhfs = await getNavigraphParsedData('vhf', search);
             const ndbs = await getNavigraphParsedData('ndb', search);
             const waypointsList = await getNavigraphParsedData('waypoints', search);
+
+            if (search === 'NEGIX') console.log(waypointsList);
 
             if (previousWaypoint && (vhfs || ndbs || waypointsList)) {
                 const vhfWaypoint = previousWaypoint && Object.entries(vhfs ?? {}).sort((a, b) => {
