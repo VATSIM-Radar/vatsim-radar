2d44702eab3d25c57fd0ff1c5ce60b8b2b7b68a5
daniluk4000 <daniluk4000@outlook.com>
2026-05-08 11:47:42 +0300

    Change Influx write options

--- app/utils/backend/influx/influx.ts
+++ app/utils/backend/influx/influx.ts
@@ -1,10 +1,27 @@
-import type { QueryApi, WriteApi } from '@influxdata/influxdb-client';
+import type { QueryApi, WriteApi, WriteOptions } from '@influxdata/influxdb-client';
 import { InfluxDB } from '@influxdata/influxdb-client';
 
 export let influxDB: InfluxDB;
 export let influxDBQuery: QueryApi;
 export let influxDBWritePlans: WriteApi;
 export let influxDBWriteMain: WriteApi;
+
+const writeOptions: Partial<WriteOptions> = {
+    batchSize: 5000,
+    flushInterval: 1000,
+    gzipThreshold: 1024 * 10,
+    maxRetries: 3,
+    maxRetryTime: 1000 * 30,
+    maxBufferLines: 100_000,
+    writeFailed(error, lines, attempt) {
+        if (attempt === 1) {
+            console.error(`Influx write failed for ${ lines.length } lines`, error);
+        }
+    },
+    writeRetrySkipped({ lines }) {
+        console.error(`Influx write retry skipped ${ lines.length } lines`);
+    },
+};
 
 export function initInfluxDB() {
     try {
@@ -14,8 +31,8 @@ export function initInfluxDB() {
         });
 
         influxDBQuery = influxDB.getQueryApi(process.env.INFLUX_ORG!);
-        influxDBWritePlans = influxDB.getWriteApi(process.env.INFLUX_ORG!, process.env.INFLUX_BUCKET_PLANS!, 'ms');
-        influxDBWriteMain = influxDB.getWriteApi(process.env.INFLUX_ORG!, process.env.INFLUX_BUCKET_MAIN!, 'ms');
+        influxDBWritePlans = influxDB.getWriteApi(process.env.INFLUX_ORG!, process.env.INFLUX_BUCKET_PLANS!, 'ms', writeOptions);
+        influxDBWriteMain = influxDB.getWriteApi(process.env.INFLUX_ORG!, process.env.INFLUX_BUCKET_MAIN!, 'ms', writeOptions);
     }
     catch (e) {
         console.error(e);
