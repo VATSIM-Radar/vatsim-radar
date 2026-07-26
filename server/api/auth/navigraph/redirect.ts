b7d3bcd30709a47fe991e73115f222b10214f457
daniluk4000 <daniluk4000@outlook.com>
2026-07-26 18:34:34 +0300

    Fix iframe redir

--- server/api/auth/navigraph/redirect.ts
+++ server/api/auth/navigraph/redirect.ts
@@ -11,7 +11,7 @@ export default defineEventHandler(async event => {
     const app = getQuery(event).app;
     const iframe = getQuery(event).iframe;
     if (app) state += '-app';
-    if (iframe) state += '-iframe';
+    if (iframe === '1') state += '-iframe';
 
     const url = new URL('https://identity.api.navigraph.com/connect/authorize');
     url.searchParams.set('client_id', config.NAVIGRAPH_CLIENT_ID);
