64f60446c9fff81f404a9bfd6c0e973f2805157e
daniluk4000 <daniluk4000@outlook.com>
2026-08-13 21:55:40 +0300

    Disable separate cache handler

--- server/api/data/vatsim/data/compact/short.ts
+++ server/api/data/vatsim/data/compact/short.ts
@@ -1,12 +1,8 @@
 import { getServerVatsimCompactShortData } from '~/utils/server/storage';
 import { validateDataReady } from '~/utils/server/h3';
 
-export default cachedEventHandler(async event => {
+export default defineEventHandler(async event => {
     if (!(await validateDataReady(event))) return;
 
     return getServerVatsimCompactShortData();
-}, {
-    name: 'short-compact',
-    maxAge: 3,
-    staleMaxAge: 2,
 });
--- server/api/data/vatsim/data/short.ts
+++ server/api/data/vatsim/data/short.ts
@@ -1,12 +1,8 @@
 import { getServerVatsimLiveShortData } from '~/utils/server/storage';
 import { validateDataReady } from '~/utils/server/h3';
 
-export default cachedEventHandler(async event => {
+export default defineEventHandler(async event => {
     if (!(await validateDataReady(event))) return;
 
     return getServerVatsimLiveShortData();
-}, {
-    name: 'short',
-    maxAge: 3,
-    staleMaxAge: 2,
 });
