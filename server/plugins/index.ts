5ac3293f949ae240e7191eeef7b559142b368549
daniluk4000 <daniluk4000@outlook.com>
2026-09-06 16:38:41 +0300

    Fix lint

--- server/plugins/index.ts
+++ server/plugins/index.ts
@@ -1,9 +1,4 @@
 import { isDataReady } from '~/utils/server/storage';
-import { prisma } from '~/utils/server/prisma';
-import { defaultRedis, getRedisSync } from '~/utils/server/redis';
-import { UserPresetType } from '#prisma';
-import type { UserBookmarkPreset } from '~/utils/server/handlers/bookmarks';
-import { toLonLat } from 'ol/proj.js';
 
 export default defineNitroPlugin(async app => {
     app.hooks.hook('request', async event => {
