881686da065c117b110d1c6032ee47b5901bc1d2
Danila Rodichkin <daniluk4000@outlook.com>
2026-09-20 19:12:27 +0300

    fix navigraph features in wrapped worlds

--- app/composables/map/world.ts
+++ app/composables/map/world.ts
@@ -1,4 +1,5 @@
 import type { Coordinate } from 'ol/coordinate.js';
+import type { Extent } from 'ol/extent.js';
 
 function modPositive(x: number, m: number) {
     return ((x % m) + m) % m;
@@ -19,4 +20,15 @@ export function getCurrentWorldCoordinate({ coordinate, eventCoordinate }: {
     const localCoordinate = modPositive((coordinate[0] + 180), 360) - 180;
 
     return [localCoordinate + (worldIndex * 360), eventCoordinate[1]];
+}
+
+export function getCurrentWorldExtent({ extent, eventCoordinate }: {
+    extent: Extent; eventCoordinate: Coordinate;
+}): Extent {
+    const currentMin = getCurrentWorldCoordinate({
+        coordinate: [extent[0], extent[1]],
+        eventCoordinate: [eventCoordinate[0], extent[1]],
+    });
+
+    return [currentMin[0], extent[1], currentMin[0] + (extent[2] - extent[0]), extent[3]];
 }
